c195196839dcdd57cac3e43cb757b8336d7e9e63
KenElg <kenneth.elgin@gmail.com>
2015-05-07 16:09:38 +0200

    redid movement/streaming with np.roll for significant speed increase

--- lattice.py
+++ lattice.py
@@ -1,87 +1,32 @@
 # Lattice movement
 import numpy as np
-import copy
 import matplotlib.pyplot as plt
-
+import copy
 
 def init_grid(Nxgrid,Nygrid,dens,blocks):
   grid=np.ones((Nxgrid,Nygrid,9),dtype=float)*dens/9
   for i in range (len(blocks)):
     grid[blocks[i,0],blocks[i,1],:]=0
   return grid
-  
-def move(grid,blocks):
-#  print 'moving'
+
+def move(grid,e):
+  for i in range(9):
+    grid[:,:,i]=np.roll(np.roll(grid[:,:,i],e[0,i],axis=0),e[1,i],axis=1)
   clonedgrid=copy.deepcopy(grid)
-  grid=np.zeros(clonedgrid.shape,dtype=float)
-  for j in xrange(0,grid.shape[0]):
-    for k in xrange(0,grid.shape[1]):  
-#      for p in range (len(blocks)):  
-#        if j==blocks[p,0] and k==blocks[p,1]:
-#          grid[j,k,0]+=0
-#          grid[j,k,1]+=clonedgrid[(j+1)%(grid.shape[0]),k-1,1]
-#          grid[j,k,2]+=clonedgrid[(j+1)%(grid.shape[0]),k+1,6]
-#          grid[j,k,3]+=clonedgrid[j,k+1,7]
-#          grid[j,k,4]+=clonedgrid[(j-1)%(grid.shape[0]),k+1,8]
-#          grid[j,k,5]+=clonedgrid[(j-1)%(grid.shape[0]),k-1,1]
-#          grid[j,k,6]+=clonedgrid[(j-1)%(grid.shape[0]),k-1,2]
-#          grid[j,k,7]+=clonedgrid[j,k-1,3]
-#          grid[j,k,8]+=clonedgrid[(j+1)%(grid.shape[0]),k-1,4]      
-        if (0<k<grid.shape[1]-1):
-          grid[j,k,0]+=clonedgrid[j,k,0]
-          grid[j,k,1]+=clonedgrid[(j-1)%(grid.shape[0]),k,1]
-          grid[j,k,2]+=clonedgrid[(j-1)%(grid.shape[0]),k-1,2]
-          grid[j,k,3]+=clonedgrid[j,k-1,3]
-          grid[j,k,4]+=clonedgrid[(j+1)%(grid.shape[0]),k-1,4]
-          grid[j,k,5]+=clonedgrid[(j+1)%(grid.shape[0]),k,5]
-          grid[j,k,6]+=clonedgrid[(j+1)%(grid.shape[0]),k+1,6]
-          grid[j,k,7]+=clonedgrid[j,k+1,7]
-          grid[j,k,8]+=clonedgrid[(j-1)%(grid.shape[0]),k+1,8]
-        elif k==0:
-          grid[j,k,0]+=0
-          grid[j,k,1]+=0
-          grid[j,k,2]+=clonedgrid[(j+1)%(grid.shape[0]),k+1,6]
-          grid[j,k,3]+=clonedgrid[j,k+1,7]
-          grid[j,k,4]+=clonedgrid[(j-1)%(grid.shape[0]),k+1,8]
-          grid[j,k,5]+=0
-          grid[j,k,6]+=0
-          grid[j,k,7]+=0
-          grid[j,k,8]+=0
-        else:
-          grid[j,k,0]+=0
-          grid[j,k,1]+=0
-          grid[j,k,2]+=0
-          grid[j,k,3]+=0
-          grid[j,k,4]+=0
-          grid[j,k,5]+=0
-          grid[j,k,6]+=clonedgrid[(j-1)%(grid.shape[0]),k-1,2]
-          grid[j,k,7]+=clonedgrid[j,k-1,3]
-          grid[j,k,8]+=clonedgrid[(j+1)%(grid.shape[0]),k-1,4]
-  for p in range (len(blocks)):  
-    grid[blocks[p,0],blocks[p,1],0]=0
-    grid[blocks[p,0],blocks[p,1],1]=0+clonedgrid[(blocks[p,0]+1)%(grid.shape[0]),blocks[p,1],5]
-    grid[blocks[p,0],blocks[p,1],2]=0+clonedgrid[(blocks[p,0]+1)%(grid.shape[0]),blocks[p,1]+1,6]
-    grid[blocks[p,0],blocks[p,1],3]=0+clonedgrid[blocks[p,0],blocks[p,1]+1,7]
-    grid[blocks[p,0],blocks[p,1],4]=0+clonedgrid[(blocks[p,0]-1)%(grid.shape[0]),blocks[p,1]+1,8]
-    grid[blocks[p,0],blocks[p,1],5]=0+clonedgrid[(blocks[p,0]-1)%(grid.shape[0]),blocks[p,1],1]
-    grid[blocks[p,0],blocks[p,1],6]=0+clonedgrid[(blocks[p,0]-1)%(grid.shape[0]),blocks[p,1]-1,2]
-    grid[blocks[p,0],blocks[p,1],7]=0+clonedgrid[blocks[p,0],blocks[p,1]-1,3]
-    grid[blocks[p,0],blocks[p,1],8]=0+clonedgrid[(blocks[p,0]+1)%(grid.shape[0]),blocks[p,1]-1,4]
-  return grid
+  for i in range(9):
+    grid[:,0,i]=clonedgrid[:,0,((i+3)%8) +1]
+    grid[:,grid.shape[1]-1,i]=clonedgrid[:,grid.shape[1]-1,((i+3)%8) +1]
+  return grid  
   
-def update(grid,relaxt,pressgradvel,blocks):
-  grid=move(grid,blocks)  
-  grid=relax(grid,relaxt,pressgradvel,blocks)
+def update(grid,relaxt,pressgradvel,blocks,e):
+  grid=move(grid,e)  
+  grid=relax(grid,relaxt,pressgradvel,blocks,e)
   return grid
   
 def calc_velocity(grid):
     velocity=np.zeros((grid.shape[0],grid.shape[1],2),dtype=float)
     velocity[:,:,0]=(1/np.sum(grid,axis=2))*(grid[:,:,1]+(grid[:,:,2]+grid[:,:,8]-grid[:,:,4]-grid[:,:,6])-grid[:,:,5])    #x-direction
     velocity[:,:,1]=(1/np.sum(grid,axis=2))*(grid[:,:,3]-grid[:,:,7]+(grid[:,:,2]+grid[:,:,4]-grid[:,:,6]-grid[:,:,8]))    #y-direction
-#    print 'x',velocity[:,:,0]
-#    print 'y',velocity[:,:,1]
-#    plt.plot(velocity[50,:,0],np.arange(velocity.shape[1]),'r+')
-#    plt.show()
     return velocity
     
 def add_pressure_grad(grid,pressgradvel,blocks):
@@ -91,19 +36,18 @@ def add_pressure_grad(grid,pressgradvel,blocks):
       velocity[blocks[p,0],blocks[p,1],0]-=pressgradvel
     return velocity
     
-def calc_eq(grid,pressgradvel,blocks):
+def calc_eq(grid,pressgradvel,blocks,e):
   velocity=add_pressure_grad(grid,pressgradvel,blocks)
   Velsqr=velocity[:,:,0]**2+velocity[:,:,1]**2
   grideq=np.zeros(grid.shape,dtype=float)
   w= np.array([4./9, 1./9, 1./36, 1./9, 1./36, 1./9, 1./36, 1./9, 1./36])
-  e = np.array([[0,1,1,0,-1,-1,-1,0,1], [0,0,1,1,1,0,-1,-1,-1]])
   Prod=np.dot(e.T,velocity.transpose(0,2,1))
   for i in xrange(0,9):
     grideq[:,:,i]=w[i]*np.sum(grid,axis=2)*(1+3* Prod[i] + 9/2.0* Prod[i]**2 -3.0/2 * Velsqr)
   return grideq
   
-def relax(grid,relaxt,pressgradvel,blocks):
-    grideq=calc_eq(grid,pressgradvel,blocks)
+def relax(grid,relaxt,pressgradvel,blocks,e):
+    grideq=calc_eq(grid,pressgradvel,blocks,e)
     grid=(1-(1/relaxt))*grid+grideq*(1/relaxt)
     return grid
     
--- main.py
+++ main.py
@@ -8,12 +8,12 @@ dens=1
 timesteps=1000
 pressgradvel=0.005
 relaxt=1.85
-blocks = np.array([[40,20]]) # Adds 1 by 1 blocks on position [x,y]
-
+blocks = np.array([]) # Adds 1 by 1 blocks on position [x,y]
+e = np.array([[0,1,1,0,-1,-1,-1,0,1], [0,0,1,1,1,0,-1,-1,-1]])
 
 grid=init_grid(Nxgrid,Nygrid,dens,blocks)
 for i in xrange(0,timesteps):
-    grid=update(grid,relaxt,pressgradvel,blocks)
+    grid=update(grid,relaxt,pressgradvel,blocks,e)
 #print grid
 velocity=calc_velocity(grid)
 plt.figure(1)
