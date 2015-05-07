692423fb6f257e2381ed5f6199870cf583f8b0ec
KenElg <kenneth.elgin@gmail.com>
2015-05-07 10:47:14 +0200

    Cleaned up code

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ for i in xrange(0,timesteps):
     grid=update(grid,relaxt,pressgradvel)
 #print grid
 velocity=calc_velocity(grid)
-plt.figure(2)
+plt.figure(1)
 plt.plot(velocity[50,:,0],np.arange(velocity.shape[1]),'r+')
+plt.xlabel('Velocity in x direction') 
+plt.ylabel('Position in y') 
+plt.title('Flow profile in pipe')
 plt.show()
